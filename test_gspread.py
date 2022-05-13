790a88c5fc3053d5133fc1a57e3d58dd23661d85
metopotato <linhstartwork@gmail.com>
2022-05-13 10:18:53 +0700

    update code

---
+++ test_gspread.py
@@ -0,0 +1,12 @@
+import gspread
+
+sa = gspread.service_account(filename='service_account.json')
+
+sh = sa.open('Test')
+
+wks = sh.worksheet("Sheet1")
+
+print('Rows: ', wks.row_count)
+print('Cols: ', wks.col_count)
+
+wks.update('A1', 'this is Linh')

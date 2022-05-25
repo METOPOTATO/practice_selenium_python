fd393ab927e24779c0c668d1545517b1c42d9653
metopotato <linhstartwork@gmail.com>
2022-05-25 15:44:24 +0700

    update

--- working_with_gspread.py
+++ working_with_gspread.py
@@ -1,7 +1,9 @@
+# https://www.youtube.com/watch?v=bu5wXjz2KvU
 import gspread
 
 sa = gspread.service_account(filename='credentials/service_account.json')
 
+# sh = sa.create('Hello')
 sh = sa.open('Test')
 
 wks = sh.worksheet("Sheet1")
@@ -10,3 +12,5 @@ print('Rows: ', wks.row_count)
 print('Cols: ', wks.col_count)
 
 wks.update('A1', 'this is Linh')
+wks.update('A2', 'this is Linh')
+wks.update('A3', 'this is Linh')
